bc3533609b52242e6585df5a5e5104a8e95ba83b
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-20 23:08:11 +0300

    Ders 32 tamamlandı.

---
+++ ApiProjeKampi.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_NavbarFormInlineAdminLayoutComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents;
+
+public class _NavbarFormInlineAdminLayoutComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
