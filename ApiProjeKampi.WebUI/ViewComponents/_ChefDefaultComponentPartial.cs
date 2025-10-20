fb7f41277dd30966a95389ae0ce69cee4f9edc5c
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-20 21:12:33 +0300

    Ders 30 tamamlandı.

---
+++ ApiProjeKampi.WebUI/ViewComponents/_ChefDefaultComponentPartial.cs
@@ -0,0 +1,24 @@
+﻿using ApiProjeKampi.WebUI.Dtos.ChefDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents;
+
+public class _ChefDefaultComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    public _ChefDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        HttpClient httpClient = _httpClientFactory.CreateClient();
+        HttpResponseMessage responseMessage = await httpClient.GetAsync("https://localhost:7051/api/Chefs");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            List<ResultChefDto>? data = await responseMessage.Content.ReadFromJsonAsync<List<ResultChefDto>>();
+            return View(data);
+        }
+        return View();
+    }
+}
