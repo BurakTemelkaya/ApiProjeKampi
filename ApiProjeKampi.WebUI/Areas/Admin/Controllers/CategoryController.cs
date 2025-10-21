8e89f3c62f94825357b2a82b100b5cd5a894a0fa
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-21 03:25:36 +0300

    Ders 37 tamamlandı.

---
+++ ApiProjeKampi.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,13 @@
+﻿using ApiProjeKampi.WebUI.Constants.Area;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.Areas.Admin.Controllers;
+
+[Area(AreaNames.Admin)]
+public class CategoryController : Controller
+{
+    public IActionResult CategoryList()
+    {
+        return View();
+    }
+}
