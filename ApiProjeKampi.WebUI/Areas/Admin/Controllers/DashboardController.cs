8d9c39a23c65956976c9a58b55b3a1566f7d7d5a
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-26 01:31:15 +0300

    Ders 69 tamamlandı.

---
+++ ApiProjeKampi.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using ApiProjeKampi.WebUI.Constants.Area;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.Areas.Admin.Controllers;
+
+[Area(AreaNames.Admin)]
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
