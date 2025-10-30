bdf98b80eb2e926a361ccfd7ae59fa9fa52a7866
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2025-10-30 22:40:16 +0300

    Ders 79 tamamlandı.

---
+++ ApiProjeKampi.WebAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,51 @@
+﻿using ApiProjeKampi.WebAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ApiProjeKampi.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StatisticsController : ControllerBase
+{
+    private readonly ApiContext _context;
+
+    public StatisticsController(ApiContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("ProductCount")]
+    public async Task<IActionResult> ProductCount(CancellationToken cancellationToken = default)
+    {
+        int count = await _context.Products.CountAsync(cancellationToken);
+
+        return Ok(count);
+    }
+
+
+    [HttpGet("ReservationCount")]
+    public async Task<IActionResult> ReservationCount(CancellationToken cancellationToken = default)
+    {
+        int count = await _context.Reservations.CountAsync(cancellationToken);
+
+        return Ok(count);
+    }
+
+    [HttpGet("ChefCount")]
+    public async Task<IActionResult> ChefCount(CancellationToken cancellationToken = default)
+    {
+        int count = await _context.Chefs.CountAsync(cancellationToken);
+
+        return Ok(count);
+    }
+
+    [HttpGet("TotalGuestCount")]
+    public async Task<IActionResult> TotalGuestCount(CancellationToken cancellationToken = default)
+    {
+        int count = await _context.Reservations.SumAsync(x => x.CountOfPeople, cancellationToken);
+
+        return Ok(count);
+    }
+}
---
+++ ApiProjeKampi.WebUI/ViewComponents/HomePageViewComponents/_HomePageStatisticsComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.HomePageViewComponents;
+
+public class _HomePageStatisticsComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _HomePageStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+
+        client.BaseAddress = new Uri("https://localhost:7051/api/Statistics/");
+
+        ViewBag.ProductCount = await client.GetFromJsonAsync<int>("ProductCount");
+
+        ViewBag.ReservationCount = await client.GetFromJsonAsync<int>("ReservationCount");
+
+        ViewBag.ChefCount = await client.GetFromJsonAsync<int>("ChefCount");
+
+        ViewBag.TotalGuestCount = await client.GetFromJsonAsync<int>("TotalGuestCount");
+
+        return View();
+    }
+}
