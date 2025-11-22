ea80b4ac34249ee06061e99ecee82735c3bd0cd9
Burak Temelkaya <temelkayaburak@gmail.com>
2025-11-22 17:40:41 +0300

    Ders 82 tamamlandı.

--- ApiProjeKampi.WebUI/Controllers/DefaultController.cs
+++ ApiProjeKampi.WebUI/Controllers/DefaultController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKampi.WebUI.Dtos.ReservationDtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ApiProjeKampi.WebUI.Controllers;
 
 public class DefaultController : Controller
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public DefaultController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpGet]
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(CreateReservationDto createReservationDto)
+    {        
+        createReservationDto.ReservationStatus = "Onay Bekliyor";
+
+        HttpClient client = _httpClientFactory.CreateClient();
+
+        HttpResponseMessage response = await client.PostAsJsonAsync("https://localhost:7051/api/Reservations", createReservationDto);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction(nameof(Index));            
+        }
+
+        return View(createReservationDto);
     }
 }
